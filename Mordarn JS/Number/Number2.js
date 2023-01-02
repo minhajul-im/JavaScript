301c6abc3aaf295a2ee568fc824a73fa17659cce
minhajul-im <minhajul.cpp@gmail.com>
2023-01-02 11:43:00 +0600

    prime number check use general

--- Mordarn JS/Number/Number2.js
+++ Mordarn JS/Number/Number2.js
@@ -17,3 +17,22 @@ const primeNumberCheckUseMathSqrt = (num) => {
 
 const prime_check = primeNumberCheckUseMathSqrt(2);
 console.log(prime_check);
+
+const primeNumberCheckGeneral = (num) => {
+  if (num === undefined || num === 1) {
+    return false;
+  } else if (num === 2) {
+    return true;
+  } else {
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+};
+
+const prime_number = primeNumberCheckGeneral(11);
+console.log(prime_number);
