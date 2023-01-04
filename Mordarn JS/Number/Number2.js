85ee0aa7fe418e1dda93e192312efd8b69c3b3a6
minhajul-im <minhajul.cpp@gmail.com>
2023-01-04 11:40:43 +0600

    prime number check use math.sqrt method

--- Mordarn JS/Number/Number2.js
+++ Mordarn JS/Number/Number2.js
@@ -2,3 +2,18 @@
  * What is prime number?
  * A prime number is a positive integer greater than 1 that has no positive divisors other than 1 and itself.
  */
+
+const primeNumberCheckUseMathSqrt = (num) => {
+  if (num === undefined || num <= 1) return false;
+
+  for (let i = 2; i < Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const primeOne = primeNumberCheckUseMathSqrt(2);
+console.log(primeOne);
