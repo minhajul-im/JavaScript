de2260b3a6cec41c51cbcbafbb3ecfc29aba2935
minhajul-im <minhajul.cpp@gmail.com>
2023-01-08 10:44:30 +0600

    get request api called

--- JSON/get-request/app.js
+++ JSON/get-request/app.js
@@ -1,9 +1,16 @@
-const getApi = async (url) => {
+const getDataByApi = async (url) => {
   try {
     const res = await fetch(url);
-    const data = res.json();
+    const data = await res.json();
+    showDataDisplayedByApiFunc(data);
     console.log(data);
   } catch (e) {
     console.log(e);
   }
+};
+
+getDataByApi(`https://jsonplaceholder.typicode.com/users`);
+
+const showDataDisplayedByApiFunc = (data) => {
+  const getDivSection = document.getElementById("body");
 };
