0a9e2a8c9bce11176f8c11d9b6fc49c1fa193c2b
minhajul-im <minhajul.cpp@gmail.com>
2023-01-11 10:21:23 +0600

    created string file

---
+++ Mordarn JS/String/StringOne.js
@@ -0,0 +1,4 @@
+/**
+ * Mutable string in Javascript
+It's important to know that in JavaScript, strings are immutable. This means that once a string is created, its contents cannot be changed. Instead, you must create a new string representing the modified version when you want to modify a string.
+ */
