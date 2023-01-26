06a4e8e41a826cd01c9911bb26c87b3613727057
minhajul-im <minhajul.cpp@gmail.com>
2023-01-26 16:15:23 +0600

    import & export files created

--- Mordarn JS/Export&Import/NameExport.js
+++
@@ -1,4 +0,0 @@
-/**
- * Named Exports:
-Named exports allow you to export multiple values (variables, functions, classes, etc.) from a module by giving each export a name. This is particularly useful when you want to expose specific pieces of functionality to other modules. You can use these named exports in other modules by importing them using the same name.
- */
