3fd6a335ce81e8088c900d2e7c7d022c6cf6fcf6
minhajul-im <minhajul.cpp@gmail.com>
2023-02-02 12:02:20 +0600

    string constructor start

---
+++ Mordarn JS/Constructor/String.js
@@ -0,0 +1,11 @@
+/**
+ * The String constructor in JavaScript works like any other constructor function. It creates new instances of the String object, allowing you to work with strings as objects with associated methods and properties. Let's break down how it works step by step:
+ */
+
+// look likes
+
+const string = new String("hello String Constructor");
+
+/**
+ * Yes, in JavaScript, instances created using the String constructor are considered objects. They are instances of the String object type. This is a core concept of JavaScript's object-oriented nature, where different data types (including strings) can be treated as objects with associated properties and methods.
+ */
