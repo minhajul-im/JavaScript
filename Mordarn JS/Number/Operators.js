2f35aef267c436e3f782dd2e5a18354a5a2807e3
minhajul-im <minhajul.cpp@gmail.com>
2023-02-06 09:17:27 +0600

    bitwise operators it's special operator

--- Mordarn JS/Number/Operators.js
+++ Mordarn JS/Number/Operators.js
@@ -46,3 +46,23 @@ let greaterEqual = x >= y; // false
 
 // Less than equal
 let lessEqual = x <= y; // true
+
+// TODO: JavaScript Bitwise Operators
+
+// Bitwise (&) operator
+let bitwiseAnd = x & y; // binary number(0,1)
+
+// Bitwise (or |) operator
+let bitwiseOr = x | y; // binary number(0,1)
+
+// Bitwise (XOR) operator
+let bitwiseXOR = x ^ y; // binary number(0,1)
+
+// Bitwise left shift
+let bitwiseLeft = x << y; // 10485760
+
+// Bitwise right shift
+let bitwiseRight = y >> x; //0
+
+// Bitwise right shift zero
+let bitwiseZero = y >>> x; // 0
