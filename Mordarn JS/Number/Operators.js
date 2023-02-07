946f6dca966c4314c0e3d230c29c9e7dd796940b
minhajul-im <minhajul.cpp@gmail.com>
2023-02-07 09:14:20 +0600

    number operators started

---
+++ Mordarn JS/Number/Operators.js
@@ -0,0 +1,27 @@
+// start number operator
+
+let x = 10,
+  y = 20;
+// Addition operator
+let addition = x + y; // 30
+
+// Subtraction operator
+let subtraction = y - x; // 10
+
+// Multiplication operator
+let multiplication = x * y; //200
+
+// Division operator
+let division = y / x; // 2
+
+// Modulus (remainder) operator
+let modulus = 21 % x; // 1
+
+// Increment operator (pre,post)
+let increment = ++x; // 11
+
+// Decrement operator (pre, post)
+let decrement = --x; // 9
+
+// Equal operator
+let equal = x == y; // false
