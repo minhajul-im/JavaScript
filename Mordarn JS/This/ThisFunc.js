54b9286e94857191464de4ad072f082a5bc15a3e
minhajul-im <minhajul.cpp@gmail.com>
2023-02-12 16:20:03 +0600

    this keyword done function file

--- Mordarn JS/This/ThisFunc.js
+++ Mordarn JS/This/ThisFunc.js
@@ -1,5 +1,4 @@
 // Explicitly Set Context:
-
 // you can use call, bind & apply
 
 const obj = {
@@ -18,6 +17,7 @@ const call = forThisCall.call(obj, "Heyoo, how's it going ");
 console.log(call);
 
 //The apply method is similar to call, but it accepts the this value as its first argument and an array (or an array-like object) of arguments as the second argument. The elements of the array are passed as individual arguments to the function.
+
 function applyFunc(hi, bye) {
   return `${hi}, I'm ${this.name} from ${this.live}. I'm ${this.age}. ${bye}....`;
 }
@@ -36,12 +36,14 @@ console.log(applyFunc.apply(obj1, arr));
  * bind:
 The bind method returns a new function with the specified this value, but it doesn't immediately execute the function. Instead, it creates a new function that, when called, will have the original function's this value permanently set to the provided value.
  */
+
 function bindFunc(hi, bye) {
   return `${hi}, I'm ${this.name} from ${this.live}. I'm ${this.age}. ${bye}....`;
 }
 
 const args = ["Hey", "have a good day..."];
 
+// bind created a function but when need your function you can access that time.
 const bind = bindFunc.bind(obj1);
 
 console.log(bind(args.at(0), args.at(1)));
