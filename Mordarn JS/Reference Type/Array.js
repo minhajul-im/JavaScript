0fd7c89a1fcccb4f0758d74acc10e4d38a1fa01b
minhajul-im <minhajul.cpp@gmail.com>
2023-04-13 16:50:10 +0600

    For of Loop on the Array

--- Mordarn JS/Reference Type/Array.js
+++ Mordarn JS/Reference Type/Array.js
@@ -1,112 +1,111 @@
 const arr = [2, 3, 4, 5, 6, 7, 8, 9];
-arr.push(10);// set last index in array.
+arr.push(10); // set last index in array.
 arr.pop(); // delete last index in array.
-arr.shift();//delete first index in array.
-arr.unshift(1);// set first index in array.
+arr.shift(); //delete first index in array.
+arr.unshift(1); // set first index in array.
 const arrSlice = arr.slice(3, 5);
 // return will new array(if you tell index number start & end).
-const arrSplice = arr.splice(1, 3,);
+const arrSplice = arr.splice(1, 3);
 // split delete, set whatever you want to do.
 const arrLength = arr.length - 1; // check for length.
-const lastIndex = arr.lastIndexOf(9); 
+const lastIndex = arr.lastIndexOf(9);
 // check index number but you can tell any any value in array return index number.
 const arrIncludes = arr.includes(9); // return boolean value.
 
 const players = [
   {
-    id: 'c6941175-cc4e-4c56-b13e-eeaabd914a51',
-    name: 'Nasir',
+    id: "c6941175-cc4e-4c56-b13e-eeaabd914a51",
+    name: "Nasir",
     age: 34,
-    contact: 'nasir@gmail.com'
+    contact: "nasir@gmail.com",
   },
   {
-    id: '3ff58ea0-4615-4225-a599-fddfe21ddbd0',
-    name: 'Mosta',
+    id: "3ff58ea0-4615-4225-a599-fddfe21ddbd0",
+    name: "Mosta",
     age: 28,
-    contact: 'mosta@gmail.com'
+    contact: "mosta@gmail.com",
   },
   {
-    id: 'd16a8fea-627c-4268-a145-958ae132f106',
-    name: 'Mash',
+    id: "d16a8fea-627c-4268-a145-958ae132f106",
+    name: "Mash",
     age: 37,
-    contact: 'mash@gmail.com'
+    contact: "mash@gmail.com",
   },
   {
-    id: '7322e8df-0b94-4f2c-ba91-b0b8ca2f7a75',
-    name: 'Tamim',
+    id: "7322e8df-0b94-4f2c-ba91-b0b8ca2f7a75",
+    name: "Tamim",
     age: 31,
-    contact: 'tamim@gmail.com'
-  }
+    contact: "tamim@gmail.com",
+  },
 ];
 
 // CRUD: C => create, R => read, U => update, D => delete.
 
 // create operation done.
 const createMehedi = {
-  id: '7f42e942-fbf2-4119-9d2f-776d496e4c40',
-  name: 'Mehedi',
+  id: "7f42e942-fbf2-4119-9d2f-776d496e4c40",
+  name: "Mehedi",
   age: 25,
-  contact: 'mehedi@gmail.com',
-}
+  contact: "mehedi@gmail.com",
+};
 players.push(createMehedi);
 // read operation done.
-const mosta = '3ff58ea0-4615-4225-a599-fddfe21ddbd0';
-const index = players.find(id => id.id === mosta);
+const mosta = "3ff58ea0-4615-4225-a599-fddfe21ddbd0";
+const index = players.find((id) => id.id === mosta);
 // update operation done.
 const updateTamim = {
-  name: 'Tamim Iqbal',
-  email: 'iqbal@gmail.com',
+  name: "Tamim Iqbal",
+  email: "iqbal@gmail.com",
 };
-const tamimId = '7322e8df-0b94-4f2c-ba91-b0b8ca2f7a75';
-const tamim = players.findIndex(id => id.id === tamimId);
+const tamimId = "7322e8df-0b94-4f2c-ba91-b0b8ca2f7a75";
+const tamim = players.findIndex((id) => id.id === tamimId);
 players[tamim] = {
   ...players[tamim],
   ...updateTamim,
-}
+};
 // delete operation start.
-const nasirId = 'c6941175-cc4e-4c56-b13e-eeaabd914a51';
-const nasirDelete = players.findIndex(id => id.id === nasirId);
+const nasirId = "c6941175-cc4e-4c56-b13e-eeaabd914a51";
+const nasirDelete = players.findIndex((id) => id.id === nasirId);
 players.splice(0, 1);
 
 //console.log(players)
 
 const persons = [
   {
-    id: 'c6941175-cc4e-4c56-b13e-eeaabd914a51',
-    name: 'Nasir',
+    id: "c6941175-cc4e-4c56-b13e-eeaabd914a51",
+    name: "Nasir",
     age: 34,
-    contact: 'nasir@gmail.com'
+    contact: "nasir@gmail.com",
   },
   {
-    id: '3ff58ea0-4615-4225-a599-fddfe21ddbd0',
-    name: 'Mosta',
+    id: "3ff58ea0-4615-4225-a599-fddfe21ddbd0",
+    name: "Mosta",
     age: 28,
-    contact: 'mosta@gmail.com'
+    contact: "mosta@gmail.com",
   },
   {
-    id: 'd16a8fea-627c-4268-a145-958ae132f106',
-    name: 'Mash',
+    id: "d16a8fea-627c-4268-a145-958ae132f106",
+    name: "Mash",
     age: 37,
-    contact: 'mash@gmail.com'
+    contact: "mash@gmail.com",
   },
   {
-    id: '7322e8df-0b94-4f2c-ba91-b0b8ca2f7a75',
-    name: 'Tamim',
+    id: "7322e8df-0b94-4f2c-ba91-b0b8ca2f7a75",
+    name: "Tamim",
     age: 31,
-    contact: 'tamim@gmail.com'
+    contact: "tamim@gmail.com",
   },
 ];
-const myMap = persons.map(id => (id?.id));// return something.
-const myFind = persons.find(name => (name?.name === 'Mosta'))
- // if you give a condition then all check then return.
-const myFIlter = persons.filter(age => (age?.age < 30));
+const myMap = persons.map((id) => id?.id); // return something.
+const myFind = persons.find((name) => name?.name === "Mosta");
+// if you give a condition then all check then return.
+const myFIlter = persons.filter((age) => age?.age < 30);
 // if you give a condition all check then return.
-persons.forEach(contact => contact?.contact);
+persons.forEach((contact) => contact?.contact);
 // no return just for something show.
-const getId = '3ff58ea0-4615-4225-a599-fddfe21ddbd0';
-const myFindIndex = persons.findIndex(id => id?.id === getId); 
+const getId = "3ff58ea0-4615-4225-a599-fddfe21ddbd0";
+const myFindIndex = persons.findIndex((id) => id?.id === getId);
 // return index number.
-
 
 // "reduce" have taken two parameters. (acc, cur) any kind of operation on "cur" value but at the end of the day return  "acc" . "cur" will value push in "acc".
 const reduces = [1, 2, 3, 4, 5, 6, 8];
@@ -123,11 +122,11 @@ const myReduce = reduces.reduce((acc, cur) => {
 }, []);
 
 const sumReduce = reduces.reduce((acc, cur) => {
-  return acc = acc + cur;
+  return (acc = acc + cur);
 }, 0);
 
 const [one, two, ...others] = reduces; //destructuring Array.
-const spread = [1, 2, 3, 4, 5, 6,];
+const spread = [1, 2, 3, 4, 5, 6];
 const spreadOperator = [...spread, 9, 10, 11];
 // when you use (...) another place that is called spread Operator.
 
@@ -135,6 +134,15 @@ const restOperator = function (a, b, ...rest) {
   //when you use (...) in the function that is called rest Operator.
   const x = a + b;
   return rest;
-}
+};
 const rest = restOperator(1, 2, 3, 4, 5, 6, 7, 8);
 
+// for of loop
+const forOfLoop = (arr) => {
+  for (const value of arr) {
+    console.log(value);
+  }
+};
+const arrForOfLoop = ["minhaj", "Jonas", "John"];
+
+console.log(forOfLoop(arrForOfLoop));
