2ef332f035e021cd3608811a4d244715675279ef
minhajul-im <minhajul.cpp@gmail.com>
2023-04-16 22:01:05 +0600

    Some files deleted & some file quick fixed

---
+++ Mordarn JS/Primitive Type/String.js
@@ -0,0 +1,9 @@
+// <-- ---------------TODO: More Example and Clear ConceptTODO:--------- -->
+
+const string = "String"; // you can any data store in string.
+const number = 1111; // you can store any number.
+
+let name = "Minhajul Islam"; //let it's a variable you can change.
+name = "Minhaj Muhammad";
+
+const names = "Minhajul, Minhaj"; // const It's a variable never change.
