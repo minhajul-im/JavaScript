1897dccf2821fb2733420bb8550dc4346128bd40
minhajul-im <minhajul.cpp@gmail.com>
2023-04-16 22:18:56 +0600

    find index on string

--- Mordarn JS/Primitive Type/String.js
+++ Mordarn JS/Primitive Type/String.js
@@ -2,12 +2,15 @@
 
 const string = "String"; // you can any data store in string.
 
-let name = "Minhajul Islam"; //let it's a variable you can change.
+//let it's a variable you can change.
+let name = "Minhajul Islam";
 name = "Minhaj Muhammad";
 
-const names = "Minhajul, Minhaj"; // const It's a variable never change.
+// const It's a variable never change.
+const names = "Minhajul, Minhaj";
 
 //  Template String You can use multi line string and you can use function and condition whatever it is.
+const str = `Bangladesh`;
 const templateString = `Hello Bangladesh, It's a small country. Many population live in Bangladesh.`;
 
 const minhaj = {
@@ -17,6 +20,20 @@ const minhaj = {
   },
   age: ["twenty"],
 };
-
 const templateStringFunc = `Hey, I'm ${minhaj?.name?.firstName}
  ${minhaj?.name?.lastName}. I'm in now ${minhaj.age[0]}`;
+
+// just for joining use this method.
+const concat = str.concat(templateString);
+
+// when you check (have it or haven't it) return Boolean.
+const includes = str.includes("d");
+
+// return index number
+const search = str.search("l");
+
+// when you want to index number.
+const indexOf = str.indexOf("d");
+
+//when you want last index number
+const lastIndexOf = str.lastIndexOf("d");
