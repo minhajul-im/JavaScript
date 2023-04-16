4acf7752f3be275a1b807d827a9ff6345ecd7097
minhajul-im <minhajul.cpp@gmail.com>
2023-04-16 22:12:10 +0600

    use of the template string

--- Mordarn JS/Primitive Type/String.js
+++ Mordarn JS/Primitive Type/String.js
@@ -1,9 +1,22 @@
 // <-- ---------------TODO: More Example and Clear ConceptTODO:--------- -->
 
 const string = "String"; // you can any data store in string.
-const number = 1111; // you can store any number.
 
 let name = "Minhajul Islam"; //let it's a variable you can change.
 name = "Minhaj Muhammad";
 
 const names = "Minhajul, Minhaj"; // const It's a variable never change.
+
+//  Template String You can use multi line string and you can use function and condition whatever it is.
+const templateString = `Hello Bangladesh, It's a small country. Many population live in Bangladesh.`;
+
+const minhaj = {
+  name: {
+    firstName: "Minhajul",
+    lastName: "Islam",
+  },
+  age: ["twenty"],
+};
+
+const templateStringFunc = `Hey, I'm ${minhaj?.name?.firstName}
+ ${minhaj?.name?.lastName}. I'm in now ${minhaj.age[0]}`;
