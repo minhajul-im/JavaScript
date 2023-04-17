4a6bf21bbb8c9c9ce4bfb052a24bcd39b69c97fb
minhajul-im <minhajul.cpp@gmail.com>
2023-04-17 16:02:27 +0600

    check is number or isn't number

--- Mordarn JS/Primitive Type/Number.js
+++ Mordarn JS/Primitive Type/Number.js
@@ -2,3 +2,7 @@
 
 // it's a number store in variable.
 const number = 1234;
+
+// "isNaN" checked, if is "number" not executed passed the next block.
+const num = 100;
+const checkIsNaN = isNaN(num) ? `Not a Number` : `It's Number`;
