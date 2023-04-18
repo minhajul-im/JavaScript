cbf01cc4c0a9e15c5212e9c8428dfe42557bf48b
minhajul-im <minhajul.cpp@gmail.com>
2023-04-18 12:25:32 +0600

    greater then equal & less then equal

--- Mordarn JS/Primitive Type/Boolean.js
+++ Mordarn JS/Primitive Type/Boolean.js
@@ -6,3 +6,9 @@ const greaterThan = num1 > num2;
 
 // less than condition.
 const lessThan = num1 < num2;
+
+// greater than or equal
+const greaterThanOrEqual = num1 >= num2;
+
+// less than or equal
+const lessThanOrEqual = num1 <= num2;
