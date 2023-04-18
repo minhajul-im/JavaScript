a3fe03ed1b3c0c0a991eef89788f6af65b691f90
minhajul-im <minhajul.cpp@gmail.com>
2023-04-18 12:31:44 +0600

    and operator & or operator

--- Mordarn JS/Primitive Type/Boolean.js
+++ Mordarn JS/Primitive Type/Boolean.js
@@ -18,3 +18,9 @@ const equalToEqual = num1 === num2;
 
 //  check not equal.
 const notEqual = num1 !== num2;
+
+// or operator check true data if it can true data return true otherwise false.
+const orCondition = num1 > 10 || num2 < 10;
+
+// and operator check true then next end of the true return true. when fast data false never go to next data.
+const andCondition = num1 === 20 && num2 > 5;
