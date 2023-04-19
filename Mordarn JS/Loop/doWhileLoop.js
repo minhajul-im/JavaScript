3efafdc50753fbaf8bd24953aa2579721df72310
minhajul-im <minhajul.cpp@gmail.com>
2023-04-19 15:47:06 +0600

    do while loop

---
+++ Mordarn JS/Loop/doWhileLoop.js
@@ -0,0 +1,8 @@
+// do while loop
+let doWhileLoop = true;
+do {
+  const execute = `firs, it executes then you can check the next condition`;
+} while (doWhileLoop);
+{
+  const statement = `above check "while loop" same condition.`;
+}
