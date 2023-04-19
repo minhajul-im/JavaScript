845c7c3bc167a3703a71973cb98d76c26e9a9e9f
minhajul-im <minhajul.cpp@gmail.com>
2023-04-19 15:58:37 +0600

    some quick fixed array file

--- Mordarn JS/Reference Type/Array.js
+++ Mordarn JS/Reference Type/Array.js
@@ -1,16 +1,31 @@
 const arr = [2, 3, 4, 5, 6, 7, 8, 9];
-arr.push(10); // set last index in array.
-arr.pop(); // delete last index in array.
-arr.shift(); //delete first index in array.
-arr.unshift(1); // set first index in array.
-const arrSlice = arr.slice(3, 5);
+
+// set last index in array.
+arr.push(10);
+
+// delete last index in array.
+arr.pop();
+
+//delete first index in array.
+arr.shift();
+
+// set first index in array.
+arr.unshift(1);
+
 // return will new array(if you tell index number start & end).
-const arrSplice = arr.splice(1, 3);
+const arrSlice = arr.slice(3, 5);
+
 // split delete, set whatever you want to do.
-const arrLength = arr.length - 1; // check for length.
-const lastIndex = arr.lastIndexOf(9);
+const arrSplice = arr.splice(1, 3);
+
+// check for length.
+const arrLength = arr.length - 1;
+
 // check index number but you can tell any any value in array return index number.
-const arrIncludes = arr.includes(9); // return boolean value.
+const lastIndex = arr.lastIndexOf(9);
+
+// return boolean value.
+const arrIncludes = arr.includes(9);
 
 const players = [
   {
@@ -48,21 +63,25 @@ const createMehedi = {
   age: 25,
   contact: "mehedi@gmail.com",
 };
-players.push(createMehedi);
+
 // read operation done.
+players.push(createMehedi);
 const mosta = "3ff58ea0-4615-4225-a599-fddfe21ddbd0";
 const index = players.find((id) => id.id === mosta);
+
 // update operation done.
 const updateTamim = {
   name: "Tamim Iqbal",
   email: "iqbal@gmail.com",
 };
+
 const tamimId = "7322e8df-0b94-4f2c-ba91-b0b8ca2f7a75";
 const tamim = players.findIndex((id) => id.id === tamimId);
 players[tamim] = {
   ...players[tamim],
   ...updateTamim,
 };
+
 // delete operation start.
 const nasirId = "c6941175-cc4e-4c56-b13e-eeaabd914a51";
 const nasirDelete = players.findIndex((id) => id.id === nasirId);
@@ -96,16 +115,22 @@ const persons = [
     contact: "tamim@gmail.com",
   },
 ];
-const myMap = persons.map((id) => id?.id); // return something.
-const myFind = persons.find((name) => name?.name === "Mosta");
+
+// return something.
+const myMap = persons.map((id) => id?.id);
+
 // if you give a condition then all check then return.
-const myFIlter = persons.filter((age) => age?.age < 30);
+const myFind = persons.find((name) => name?.name === "Mosta");
+
 // if you give a condition all check then return.
-persons.forEach((contact) => contact?.contact);
+const myFIlter = persons.filter((age) => age?.age < 30);
+
 // no return just for something show.
+persons.forEach((contact) => contact?.contact);
+
+// return index number.
 const getId = "3ff58ea0-4615-4225-a599-fddfe21ddbd0";
 const myFindIndex = persons.findIndex((id) => id?.id === getId);
-// return index number.
 
 // "reduce" have taken two parameters. (acc, cur) any kind of operation on "cur" value but at the end of the day return  "acc" . "cur" will value push in "acc".
 const reduces = [1, 2, 3, 4, 5, 6, 8];
@@ -125,24 +150,16 @@ const sumReduce = reduces.reduce((acc, cur) => {
   return (acc = acc + cur);
 }, 0);
 
-const [one, two, ...others] = reduces; //destructuring Array.
+//destructuring Array.
+const [one, two, ...others] = reduces;
+
+// when you use (...) another place that is called spread Operator.
 const spread = [1, 2, 3, 4, 5, 6];
 const spreadOperator = [...spread, 9, 10, 11];
-// when you use (...) another place that is called spread Operator.
 
+//when you use (...) in the function that is called rest Operator.
 const restOperator = function (a, b, ...rest) {
-  //when you use (...) in the function that is called rest Operator.
   const x = a + b;
   return rest;
 };
 const rest = restOperator(1, 2, 3, 4, 5, 6, 7, 8);
-
-// for of loop
-const forOfLoop = (arr) => {
-  for (const value of arr) {
-    console.log(value);
-  }
-};
-const arrForOfLoop = ["minhaj", "Jonas", "John"];
-
-console.log(forOfLoop(arrForOfLoop));
