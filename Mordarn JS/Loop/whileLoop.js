e2ea906a9df505cfbca800d9f2d92b43c001ab53
minhajul-im <minhajul.cpp@gmail.com>
2023-04-19 15:46:03 +0600

    while loop

---
+++ Mordarn JS/Loop/whileLoop.js
@@ -0,0 +1,8 @@
+//while loop
+let whileLoop = true;
+while (whileLoop) {
+  const statement = `when the condition is true he continues infinitely he doesn't know when I'd stop. that's why the condition in your hand if you want to stop then use the break method or false.`;
+
+  whileLoop = false;
+  break;
+}
