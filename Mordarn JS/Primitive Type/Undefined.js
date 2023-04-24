37585538379dfc5fe86d594811956e4fee45466e
minhajul-im <minhajul.cpp@gmail.com>
2023-04-24 16:54:24 +0600

    why give me undefined on object

--- Mordarn JS/Primitive Type/Undefined.js
+++ Mordarn JS/Primitive Type/Undefined.js
@@ -1,5 +1,9 @@
 //start undefined
 
-// When a variable is declared without being assigned a value, its value is "undefined" by default.
+// 1.TODO: When a variable is declared without being assigned a value, its value is "undefined" by default.
 let str;
 console.log(str);
+
+// 1.TODO: When an object property that does not exist is accessed, the returned value is "undefined".
+const object = {};
+console.log(object.name);
