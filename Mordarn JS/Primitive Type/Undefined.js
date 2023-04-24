2559a25112462e12d90cff76a09b6a53c6547823
minhajul-im <minhajul.cpp@gmail.com>
2023-04-24 16:56:47 +0600

    why give me return undefined on function

--- Mordarn JS/Primitive Type/Undefined.js
+++ Mordarn JS/Primitive Type/Undefined.js
@@ -4,6 +4,10 @@
 let str;
 console.log(str);
 
-// 1.TODO: When an object property that does not exist is accessed, the returned value is "undefined".
+// 2.TODO: When an object property that does not exist is accessed, the returned value is "undefined".
 const object = {};
 console.log(object.name);
+
+// 3.TODO: If a function does not explicitly return a value, the returned value is "undefined" by default.
+const undeFined = () => {};
+console.log(undeFined());
