6bba68820c6f08ab13192df34c50afd72383eb2a
minhajul-im <minhajul.cpp@gmail.com>
2023-05-04 13:24:28 +0600

    implement null

--- Mordarn JS/Primitive Type/Null.js
+++ Mordarn JS/Primitive Type/Null.js
@@ -1,7 +1,7 @@
 /*
 What is null? 
 "null" is a special value that represents an empty or unknown value.
-null type of object.
+null type of object & false value.
 
 When a variable is assigned to null, it means that the variable has no value or points to no object. It is often used to represent a non-existent or invalid object or value. For example, if you have a variable that should point to an object but you don't have an object to assign to it, you can initialize the variable to null to indicate that it does not currently point to any object.
 */
@@ -10,3 +10,10 @@ console.log(typeof null);
 let nullValue = null;
 nullValue = 10;
 console.log(nullValue);
+
+let x = null;
+
+if (x) {
+  // never execute because null is a false value
+  console.log(nullValue);
+}
