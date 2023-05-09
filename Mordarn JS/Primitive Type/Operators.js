8fdaa0331f31d22f8f5d2d95c6ed485923633629
minhajul-im <minhajul.cpp@gmail.com>
2023-05-09 13:43:01 +0600

    increment & decrement operator

--- Mordarn JS/Primitive Type/Operators.js
+++ Mordarn JS/Primitive Type/Operators.js
@@ -1,16 +1,22 @@
 // Addition operator
 let x = 10,
   y = 20;
-let addition = x + y;
+let addition = x + y; // 30
 
 // Subtraction operator
-let subtraction = y - x;
+let subtraction = y - x; // 10
 
 // Multiplication operator
-let multiplication = x * y;
+let multiplication = x * y; //200
 
 // Division operator
-let division = y / x;
+let division = y / x; // 2
 
 // Modulus (remainder) operator
-let modulus = 21 % x;
+let modulus = 21 % x; // 1
+
+// Increment operator (pre,post)
+let increment = ++x; // 11
+
+// Decrement operator (pre, post)
+let decrement = --x; // 9
