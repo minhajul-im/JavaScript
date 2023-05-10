61bf91359c061ef6573354426fd29ad2cb01b61c
minhajul-im <minhajul.cpp@gmail.com>
2023-05-10 20:21:27 +0600

    if else conditions

---
+++ Mordarn JS/Primitive Type/Conditions.js
@@ -0,0 +1,17 @@
+//TODO: JavaScript If-else
+
+// If condition
+if (conditions) {
+  // if conditions true then execute this block
+} else {
+  // if conditions false then execute this block
+}
+
+// else if conditions
+if (conditions) {
+  // if conditions true then execute this block
+} else if (conditions) {
+  // if conditions true then execute this block
+} else {
+  // if all conditions false above then executed this block
+}
