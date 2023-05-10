d5940b21ce34a6325eb7a92d1ede247ccf081a3f
minhajul-im <minhajul.cpp@gmail.com>
2023-05-10 14:06:06 +0600

    special ternary & chaining operator

--- Mordarn JS/Primitive Type/Operators.js
+++ Mordarn JS/Primitive Type/Operators.js
@@ -79,3 +79,25 @@ let orOperator = 10 == 20 || 20 == 33; // false
 
 // Logical Not (!)
 let notLogical = !(10 == 20); // true
+
+//TODO: JavaScript Special Operators
+
+// Conditional ternary operator (__? true : false)
+let ternaryOperator = x >= y ? true : false; // false
+
+// Chaining Operator check if yes or not then use Chaining Operator.
+const chainingObj = {
+  name: {
+    firstName: "Minhajul",
+    lastName: "Islam",
+  },
+  age: "TwentyOne",
+};
+
+// example chaining operator on implement object.
+const chainingOperatorObj = chainingObj?.name?.lastName;
+
+//if there is no index  then return undefined not return error.
+// example chaining operator on implement array.
+const chainingArr = [10, 20, 30, 40];
+const chainingOperatorArr = chainingArr?.at();
