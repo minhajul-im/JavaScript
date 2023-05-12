4e71d41357357b68514f1c5edb00453351998060
minhajul-im <minhajul.cpp@gmail.com>
2023-05-12 18:39:43 +0600

    number file updated

--- Mordarn JS/Primitive Type/Number.js
+++ Mordarn JS/Primitive Type/Number.js
@@ -1,57 +1,58 @@
-//TODO: <-- ----------------Number --------------------- -->a
-
 // it's a number store in variable.
 const number = 1234;
 
 // "isNaN" checked, if is "number" not executed passed the next block.
-const num = 100;
-const checkIsNaN = isNaN(num) ? `Not a Number` : `It's Number`;
+let num = 100;
+const checkIsNaN = isNaN(num) ? false : true; // true
 
 // find minimum numbers
-const minNumber = Math.min(12, 23, 40, 50);
+const minNumber = Math.min(12, 23, 40, 50); // 12
 
 // find maximum number
-const maxNumber = Math.max(20, 10, 40, 20);
+const maxNumber = Math.max(20, 10, 40, 20); //40
 
 //the square root of a number
-const sqrtNumber = Math.sqrt(20);
+const sqrtNumber = Math.sqrt(20); // 4.47
 
 //A power, or index, is used when we want to multiply a number by itself several times
-const powNumber = Math.pow(5, 2);
+const powNumber = Math.pow(5, 2); // 25
 
 // generate random number
-const random = Math.random() * num;
+const random = Math.random() * num; // 20.45
 
 // return integer number
-const parseInt = Number.parseInt(num);
+const parseInt = Number.parseInt(num); // 100
 
 //return integer number doesn't exist dot after number.
-const trunc = Math.trunc(-0.69);
-const floor = Math.floor(-0.6);
+const trunc = Math.trunc(-0.69); // -0
+const floor = Math.floor(-0.6); // -1
 
 // return integer but .5 up increment and .5 down that number.
-const round = Math.round(50.5);
+const round = Math.round(50.5); // 51
 
 // return absolute value.
-const abs = Math.abs(random);
+const abs = Math.abs(random); // 100
 
 //increment number
 let pre = 10;
-let preIncrement = ++pre;
-let postIncrement = pre++;
+let preIncrement = ++pre; // 11
+let postIncrement = pre++; // 11
 
 //decrement number
-let preDecrement = --pre;
-let postDecrement = pre--;
+let preDecrement = --pre; // 10
+let postDecrement = pre--; // 10
 
-// return not with decimal.
-const parseInteger = parseInt(num);
+// check finite number return boolean
+const finite = Number.isFinite(num); // true
 
 // return with decimal.
-const parseFloating = parseFloat(num);
+const parseFloating = parseFloat(num); // 100.19
 
 // return decimal numbers what you want two digits or as many numbers if you want.
-const toFixedNumber = num.toFixed(2);
+const toFixedNumber = num.toFixed(2); // 100.19
+
+//It returns the string that represents the exponential notation of the given number.
+const exponential = num.toExponential(); //1e+2
 
 //given one max number but generate two random number return function two generated number, now you have gotten two generate number you can any kind operation .
 const generateRand = (max, whatDoYouWant) => {
