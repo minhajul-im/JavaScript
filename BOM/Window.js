54b8f95f2d9ad891928a73bfb22ffbfd74a2cd1b
minhajul-im <minhajul.cpp@gmail.com>
2023-05-12 19:11:44 +0600

    window confirm & prompt implement

--- BOM/Window.js
+++ BOM/Window.js
@@ -1,4 +1,10 @@
 //The default object of browser is window means you can call all the functions of window by specifying window or directly.
 
-// alert message
+// displays the alert box containing message with ok button.
 window.alert("hello Bangladesh");
+
+//	displays the confirm dialog box containing message with ok and cancel button.
+window.confirm("hello Bangladesh");
+
+//displays a dialog box to get input from the user.
+window.prompt();
