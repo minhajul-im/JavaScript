9f54fb83d510f79f23f9e13a0445a8e7a7c96a35
minhajul-im <minhajul.cpp@gmail.com>
2023-05-12 17:54:51 +0600

    hours & years implement

--- Mordarn JS/Date/Date.js
+++ Mordarn JS/Date/Date.js
@@ -7,14 +7,20 @@ const milliSecond = time.getMilliseconds();
 // it's return number between 0 to 59 that represents that's on local time
 const second = time.getSeconds();
 
-// it's return number between 0-59 that represents that's on local time
+// it's return number between 0 to 59 that represents that's on local time
 const minute = time.getMinutes();
+
+// it's return number between 0 to 23 that represents that's on local time
+const hour = time.getHours();
 
 // it's return number between 0 to 30 that represents that's on local time
 const data = time.getDate();
 
 // it's return number between 0 to 6 that represents that's week, but week start (monday)
-const day = time.getDay();
+const dayWeek = time.getDay();
 
 // it's return number current month between 0 to 11 that represent that's on local time.
 const month = time.getMonth();
+
+// it's return year, if you want to current or past
+const years = time.getFullYear();
