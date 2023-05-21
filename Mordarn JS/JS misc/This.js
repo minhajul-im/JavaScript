994923f29a558fa324bd2f8069981a924de5a6d4
minhajul-im <minhajul.cpp@gmail.com>
2023-05-21 17:37:06 +0600

    js this keyword use constructor and call, apply & bind

--- Mordarn JS/JS misc/This.js
+++ Mordarn JS/JS misc/This.js
@@ -1,4 +1,4 @@
-/**
+/*
  “This” keyword refers to an object that is executing the current piece of code. It references the object that is executing the current function. If the function being referenced is a regular function, “this” references the global object,
 */
 
@@ -28,3 +28,38 @@ const obj = {
 };
 console.log(obj.useThis());
 console.log(obj?.anotherObj?.inSideObj?.nestedObj?.nestedFn());
+
+//Constructor function
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+/*
+ When a function is used as a constructor function using the new keyword, this refers to the newly created instance of the object being constructed.
+ */
+const person = new Person(`John`, 21);
+console.log(`Hello, I'm ${person.name} & I'm ${person.age}`);
+
+// you can use call, bind & apply
+const obj1 = {
+  name: "John",
+  age: 25,
+  live: "Dhaka",
+};
+const obj2 = {
+  name: "Jonas",
+  age: 24,
+  country: "USA",
+};
+
+function useThis() {
+  // console.log(`Hello ${this.name} from ${this.live}, I'm ${this.age}.`); // call
+  console.log(`Hello ${this.name} from ${this.country}, I'm ${this.age}.`); // apply
+}
+useThis.call(obj1); // call only call
+
+useThis.apply(obj2); // apply only call
+
+// "bind" is slightly different, you have to store in a variable and then call.
+const bind = useThis.bind(obj2);
+bind();
