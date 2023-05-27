4e7fc9e3303db629898ae58ab268d02f5d06b27a
minhajul-im <minhajul.cpp@gmail.com>
2023-05-27 22:06:07 +0600

    Map vs Object article

---
+++ Mordarn JS/Object/MapVsObj.js
@@ -0,0 +1,22 @@
+/**
+ * new Map vs Object
+ * Data Structure:-
+ *  myObject = {}, creates object literal syntax.
+ *  myMap = new Map(), creates hash-table data structure.
+ *
+ * Key Type & Key:-
+ * myObject, keys in an object are limited string & symbols.
+ * myMap, keys in a map can be any datatype, including object, function, number or boolean.
+ *
+ * Key Uniqueness:-
+ * myObject, Object properties can have duplicate keys. The last assigned value for a key will be the one that remains.
+ * myMap, Map keys are unique. If you try to add a duplicate key, it will overwrite the existing value.
+ *
+ * Built-in Method:-
+ * myObject,Object have built-in methods like as, Object.keys/values/entries(), delete().
+ * myMap,: Maps provide specific methods like set(), get(), has(), delete(), clear() and size for working with key-value pairs.
+ *
+ * Iteration:-
+ * myObject, for in loop or for of loop.
+ * myMap, for of loop or foreach.
+ */
