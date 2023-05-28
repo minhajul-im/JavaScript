3959f488feb03402c5d40de686e42a9b31f25a17
minhajul-im <minhajul.cpp@gmail.com>
2023-05-28 12:57:26 +0600

    Syntax error

---
+++ Mordarn JS/Error/Syntax.js
@@ -0,0 +1,27 @@
+/**
+ * What is syntax error?
+  A syntax error in programming, including in JavaScript, occurs when the code violates the rules of the language's syntax. In other words, the code structure or syntax is incorrect, making it impossible for the JavaScript engine to parse and understand the code.
+  
+  Syntax errors can be caused by various mistakes, such as missing or misplaced brackets, parentheses, semicolons, quotation marks, or incorrect use of keywords or operators. They are typically detected during the parsing phase, before the code is executed.
+
+  if you make a mistake syntax will give an error.
+ */
+
+// const x 'hello';
+
+// const syn = = 'error';
+
+// console.log('error';
+
+// const object {
+//   name: 'John',
+//   age: 30,
+// };
+
+// const array = ['John', 'Doe', 'Smith';
+
+// const null = null;
+
+// cosnt num = 20;
+
+// const number = 3o;
