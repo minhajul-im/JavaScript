6f2ee1b07b2373be92b9a00c35e0989001bd2571
minhajul-im <minhajul.cpp@gmail.com>
2023-05-30 21:08:14 +0600

    window screen height & width

--- Browser/Bom/Window-Properties.js
+++ Browser/Bom/Window-Properties.js
@@ -1,14 +1,28 @@
 // window open and close
 const openBtn = document.getElementById("open-btn");
 const closeBtn = document.getElementById("close-btn");
-let windows;
+let newTab;
 
 // open
 openBtn.addEventListener("click", () => {
-  windows = window.open("https://www.google.com/");
+  newTab = window.open("https://www.google.com/");
 });
 
 // close
 closeBtn.addEventListener("click", () => {
-  window.close(windows);
+  window.close(newTab);
+});
+
+// window screen height & width
+const heightBtn = document.getElementById("height-btn");
+const widthBtn = document.getElementById("width-btn");
+
+// screen height
+heightBtn.addEventListener("click", () => {
+  document.getElementById("height-span").innerHTML = window.innerHeight;
+});
+
+// screen width
+widthBtn.addEventListener("click", () => {
+  document.getElementById("width-span").innerHTML = window.innerWidth;
 });
