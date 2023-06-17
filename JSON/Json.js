1b1599046caa6dd7e59ea9a44df6217b29c894b2
minhajul-im <minhajul.cpp@gmail.com>
2023-06-17 11:21:04 +0600

    JSON Start

---
+++ JSON/Json.js
@@ -0,0 +1,22 @@
+// Start json
+
+/**
+ * What is json?
+ *JSON (JavaScript Object Notation) is a lightweight data interchange format that is commonly used for transmitting data between a server and a web application, as well as for storing and structuring data. It is designed to be easy for humans to read and write and easy for machines to parse and generate.
+
+ JSON represents data in a key-value pair format, similar to how objects are represented in JavaScript.
+ 
+ Here some example:-
+ */
+
+const json = {
+  name: "John",
+  id: 1,
+  age: 25,
+  address: {
+    street: "123 Main St",
+    city: "New York",
+    postalCode: "10001",
+  },
+  hobbies: ["reading", "playing guitar", "hiking"],
+};
