6d42c874a91d34a1b09452b98b23dd6df4ddd140
minhajul-im <minhajul.cpp@gmail.com>
2023-06-21 22:46:53 +0600

    Async file update

---
+++ Mordarn JS/Async/Async.js
@@ -0,0 +1,9 @@
+const getRequest = async (url) => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
