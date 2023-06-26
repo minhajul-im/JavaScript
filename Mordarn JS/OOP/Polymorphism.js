9d3312d804c52402a3568c5027c04bffc98d44aa
minhajul-im <minhajul.cpp@gmail.com>
2023-06-26 19:54:24 +0600

    oop start polymorphism

---
+++ Mordarn JS/OOP/Polymorphism.js
@@ -0,0 +1,7 @@
+/**
+ * What is Polymorphism?
+ * 
+  Polymorphism is a concept in object-oriented programming that allows objects of different classes to be treated as objects of a common parent class. It enables a single interface to be used to represent multiple related classes.
+
+  In polymorphism, objects can be passed or accessed through a common interface, such as a shared method or property, even though they may have different implementations for that interface.
+ */
