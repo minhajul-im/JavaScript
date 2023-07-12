59c5e47d4460916f097792738ae2a6a545e5de58
minhajul-im <minhajul.cpp@gmail.com>
2023-07-12 14:10:17 +0600

    string immutabel methods start

---
+++ Mordarn JS/String/immutable.js
@@ -0,0 +1,6 @@
+//  Template String You can use multi line string and you can use function and condition whatever it is.
+const str = `Bangladesh`;
+const templateString = `Hello Bangladesh, It's a small country. Many population live in Bangladesh.`;
+
+// just for joining use this method.
+const concat = str.concat(templateString);
