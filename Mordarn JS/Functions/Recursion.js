cd7dd024197bfa608c2f5083498b6052b48d0ae1
minhajul-im <minhajul.cpp@gmail.com>
2023-08-07 21:02:46 +0600

    base case & recursive

--- Mordarn JS/Functions/Recursion.js
+++ Mordarn JS/Functions/Recursion.js
@@ -1,6 +1,10 @@
 /**
  * What is recursion?
  * Recursion refers to the process of a function calling itself as part of its execution. It's a powerful programming technique used to solve problems that can be broken down into smaller, similar sub-problems. Recursion is widely used in various algorithms and tasks, such as traversing data structures, solving mathematical problems, and more...
+ * 
+ * Base Case: This is the termination condition that specifies when the recursion should stop. Without a base case, the recursive function would continue to call itself indefinitely, leading to a stack overflow error.
+
+Recursive Case: In this part of the function, it calls itself with a modified version of the problem, making the problem smaller and eventually reaching the base case.
  */
 
 const x = [10, 20, 30];
