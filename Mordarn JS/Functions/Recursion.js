da307c8e9e6419d213af8966e405f4abe3610575
minhajul-im <minhajul.cpp@gmail.com>
2023-08-07 21:02:11 +0600

    what is recursion

--- Mordarn JS/Functions/Recursion.js
+++ Mordarn JS/Functions/Recursion.js
@@ -1,6 +1,10 @@
-var x = [10, 20, 30];
+/**
+ * What is recursion?
+ * Recursion refers to the process of a function calling itself as part of its execution. It's a powerful programming technique used to solve problems that can be broken down into smaller, similar sub-problems. Recursion is widely used in various algorithms and tasks, such as traversing data structures, solving mathematical problems, and more...
+ */
 
-// recursion function
+const x = [10, 20, 30];
+
 function recursive(x, i) {
   if (i === x.length - 1) {
     return x[i];
@@ -8,5 +12,5 @@ function recursive(x, i) {
 
   return x[i] + recursive(x, i + 1);
 }
-var sum = recursive(x, 0);
+const sum = recursive(x, 0);
 console.log(sum);
