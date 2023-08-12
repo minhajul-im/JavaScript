eaa1ad9998fde09f1828fe25fe345c7ce082a4c5
minhajul-im <minhajul.cpp@gmail.com>
2023-08-12 17:12:05 +0600

    variable fancy type things

---
+++ Mordarn JS/Fancy/variableFancy.js
@@ -0,0 +1,4 @@
+// JavaScript  you were using the comma operator within parentheses. In JavaScript, the comma operator evaluates each of its operands and returns the value of the last operand.
+
+// can you guess which is one is return,
+const sum = (10, 20, 30); // 30
