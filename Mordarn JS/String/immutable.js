18f544d953f8d90d6b1facfeac7d310b01ccde34
minhajul-im <minhajul.cpp@gmail.com>
2023-08-19 09:59:16 +0600

    string uppercase immutable

--- Mordarn JS/String/immutable.js
+++ Mordarn JS/String/immutable.js
@@ -7,13 +7,16 @@ const length = str.length; // 16
 const templateString = `Hello Bangladesh, It's a small country. Many population live in Bangladesh.`;
 
 //includes(): This method returns a boolean indicating whether or not the specified substring is contained in the current string.
-const includes = str.includes("d");
+const includes = str.includes("d"); // true
 
 //search(): This method returns the index of the first occurrence of the specified regular expression in the current string.
-const search = str.search("l");
+const search = str.search("l"); // 2
 
 //replaceAll(): This method replaces all occurrences of the specified regular expression with the specified replacement string.
-const replaceAll = templateString.replaceAll(`Bangladesh`, `Dhaka`);
+const replaceAll = templateString.replaceAll(`Bangladesh`, `Dhaka`); //Hello Dhaka, It's a small country. Many population live in Dhaka.
 
 //toLocaleLowerCase(): This method converts the current string to lowercase, using the current locale.
-const lowercase = str.toLocaleLowerCase();
+const lowercase = str.toLocaleLowerCase(); // hello bangladesh
+
+// toLocaleUpperCase(): This method converts the current string to uppercase, using the current locale.
+const uppercase = str.toUpperCase(); // HELLO BANGLADESH
