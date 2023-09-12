e57a52758d8fb6c8dc08aee361db6428b2afcadc
minhajul-im <minhajul.cpp@gmail.com>
2023-09-12 21:51:50 +0600

    async function example

--- Mordarn JS/Async/Async.js
+++ Mordarn JS/Async/Async.js
@@ -2,3 +2,9 @@
  * What is async?
  * async is a keyword in javascript & when creating an async function then need the async. an async function always returns value inside the wrap of the promise.
  */
+
+const asyncFunc = async () => {
+  return "Return value inside the wrap of the promise!";
+};
+
+asyncFunc(); //Promise {<fulfilled>: 'Return value inside the wrap of the promise!'}
