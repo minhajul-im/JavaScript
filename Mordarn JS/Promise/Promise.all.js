a61208187d936ebe5bfc03414942902224328aeb
minhajul-im <minhajul.cpp@gmail.com>
2023-09-24 18:42:33 +0600

    promise.all metheod updated

--- Mordarn JS/Promise/Promise.all.js
+++ Mordarn JS/Promise/Promise.all.js
@@ -4,30 +4,35 @@
  *  but if anyone's promise is rejected then all promises are rejected. that is the concept.
  */
 
-const promise1 = new Promise((resolve, reject) => {
+const promise1 = new Promise((resolve, _reject) => {
   setTimeout(() => resolve("promise1"), 1000);
 });
 
 const promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => reject("promise2"), 3000);
+  setTimeout(() => resolve("promise2"), 3000); // resolve
+  // setTimeout(() => reject("promise2"), 3000); // reject
 });
 
-const promise3 = new Promise((resolve, reject) => {
+const promise3 = new Promise((resolve, _reject) => {
   setTimeout(() => resolve("promise3"), 6000);
 });
 
 const allPromises = Promise.all([promise1, promise2, promise3]);
-console.log(allPromises); // actually its return a promise
+// console.log(allPromises); // actually it'qs return a promise
 
 /**
  *  if all promises are resolved //  ['promise1', 'promise2', 'promise3']
  *  if anyone's promise is rejected// Uncaught (in promise) promise2
  */
 
-allPromises.then((value) => console.log(value));
+// allPromises.then((value) => console.log(value));
 
 const asyncFunc = async () => {
-  const res = await allPromises;
-  console.log(res);
+  try {
+    const res = await allPromises;
+    console.log(res);
+  } catch (error) {
+    console.error(error);
+  }
 };
 asyncFunc();
