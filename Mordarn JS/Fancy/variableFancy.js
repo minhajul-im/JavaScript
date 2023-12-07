9360ac4e4ed3081f9b6b76ccd7b4c683b697b9c7
minhajul-im <minhajul.cpp@gmail.com>
2023-12-07 08:12:24 +0600

    crated recursion folder

--- Mordarn JS/Fancy/variableFancy.js
+++ Mordarn JS/Fancy/variableFancy.js
@@ -1,4 +1,5 @@
-// JavaScript  you were using the comma operator within parentheses. In JavaScript, the comma operator evaluates each of its operands and returns the value of the last operand.
+// JavaScript  you were using the comma operator within parentheses. In JavaScript, the
+// comma operator evaluates each of its operands and returns the value of the last operand.
 
 // can you guess which is one is return,
 const sum = (10, 20, 30); // 30
